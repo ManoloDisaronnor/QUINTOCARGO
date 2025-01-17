daf2d6761b59d6ead68c4551e1e21a4e002dfa27
Manuel Santos Márquez <xmsanmar318@ieshnosmachado.org>
2025-01-17 09:43:54 +0100

    Modificada las vistas de cliente y añadido algunos campos

--- addons/quinto_cargo/models/persona.py
+++ addons/quinto_cargo/models/persona.py
@@ -5,7 +5,9 @@ class persona(models.Model):
     _description = 'Persona'
 
     dni = fields.Char('DNI', required=True, help='Introduzca su DNI', size = 9)
+    foto = fields.Binary('Foto', help='Introduzca su foto')
     nombre = fields.Char('Nombre', required=True, help='Introduzca su nombre', size = 25)
+    apellidos = fields.Char('Apellidos', required=True, help='Introduzca sus apellidos', size = 50)
     direccion = fields.Char('Dirección', help='Introduzca su dirección', size = 60)
     telefono = fields.Integer('Teléfono', required=True, help='Introduzca su teléfono', digits = (9))
     email = fields.Char('Email', required=True, help='Introduzca su email', size = 50)
