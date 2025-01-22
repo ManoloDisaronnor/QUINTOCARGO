e82e5ccb78e9659a074818ba8d189d031709dd8b
Manuel Santos Márquez <xmsanmar318@ieshnosmachado.org>
2025-01-22 19:44:04 +0100

    Cambiado el menu

--- addons/quinto_cargo/models/empleado.py
+++ addons/quinto_cargo/models/empleado.py
@@ -13,3 +13,14 @@ class empleado(models.Model):
     fecha_contratacion = fields.Date('Fecha de contratación', required=True)
     sueldo = fields.Float('Sueldo', required=True, digits = (10,2))
     horas_trabajadas = fields.Float('Horas trabajadas', required=True, digits = (5,2))
+
+    # Definir la acción para abrir la vista de formulario en vista Kanban
+    def action_open(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Cliente',
+            'view_mode': 'form',
+            'res_model': 'quintocargo.cliente',
+            'res_id': self.id,
+            'target': 'current',
+        }
